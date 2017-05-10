1aadc7c612639ac8d14faa920524b1b64237f034
Joost van Bergen <joostvanbergen@live.nl>
2017-05-10 10:44:22 +0200

    Landing Page
    Begonnen met de landing

--- index.php
+++ index.php
@@ -8,10 +8,34 @@ and open the template in the editor.
     <head>
         <meta charset="UTF-8">
         <title>Weten van Vergeten</title>
+        
+        <link href="assets/css/global.css" rel="stylesheet" type="text/css" />
+        <link href="assets/css/landing.css" rel="stylesheet" type="text/css" />
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+        <section id="landing">
+            <div id="content">
+                <div id="app">
+                    
+                </div>
+                <div id="text-wrapper">
+                    <div id="text">
+                        <img id="logo" src="assets/img/logo-slogan.png" alt="Logo met Slogan" />
+                        <p>
+                            Weten van Vergeten biedt u een uitkomst in een onzekere
+                            periode, voor u en uw familieleden.
+                        </p>
+                        <p>
+                            Download nu de app via de Google Play Store
+                            of via de Apple App Store.
+                        </p>
+                    </div>
+                    <div id="action-buttons">
+                        <a>Play Store</a>
+                        <a>App Store</a>
+                    </div>
+                </div>
+            </div>
+        </section>
     </body>
 </html>
